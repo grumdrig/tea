b7933af9632116afbe52fcbd07165ea02e243cff
Eric Fredricksen <eric@fredricksen.net>
2010-12-20 00:26:29 +0000

    Change usage paradigm
    git-svn-id: svn+ssh://grumdrig.com/svn/trunk/src/tea@1232 b1efd146-170f-0410-8dc8-cea33ec6b4df

--- tea.py
+++ tea.py
@@ -51,20 +51,20 @@ def decrypt(v0, v1, key, rounds=32):
 
 def encipher(s, key):
   """TEA-encipher a string"""
-  assert struct.calcsize('L') == 4
+  assert struct.calcsize('I') == 4
   s = s.ljust(8 * int((len(s) + 7)/8), '\x00')  # pad with 0's
-  u = struct.unpack('%dL' % (len(s) / 4), s)
+  u = struct.unpack('%dI' % (len(s) / 4), s)
   e = [encrypt(u[i],u[i+1], key) for i in range(len(u))[::2]]
-  return ''.join([struct.pack('2L', ee,ef) for ee,ef in e])
+  return ''.join([struct.pack('2I', ee,ef) for ee,ef in e])
     
 
 def decipher_raw(s, key):
   """TEA-decipher a raw string"""
-  assert struct.calcsize('L') == 4
+  assert struct.calcsize('I') == 4
   assert len(s) % 8 == 0, len(s)
-  u = struct.unpack('%dL' % (len(s) / 4), s)
+  u = struct.unpack('%dI' % (len(s) / 4), s)
   e = [decrypt(u[i],u[i+1], key) for i in range(len(u))[::2]]
-  return ''.join([struct.pack('2L', ee,ef) for ee,ef in e])
+  return ''.join([struct.pack('2I', ee,ef) for ee,ef in e])
 
 def decipher(s, key):
   """TEA-decipher a readable string"""
@@ -72,20 +72,22 @@ def decipher(s, key):
 
 
 def main():
-  """Usage: tea.py [OPTS] KEY [INFILE [OUTFILE]]
+  """Usage: tea.py [OPTS] [KEY] [INFILE [OUTFILE]]
 OPTS:
   -d       Decipher (default is to encipher)
   -h       Ciphertext is expressed in hex format (default for a tty)
-  -p       Ciphertext is expressed as literal text (default otherwise)
+  -t       Ciphertext is expressed as literal text (default otherwise)
 KEY:
-  Either a key or key file is required.
-  -k KEY   A key string of length exactly 16
-  KEYFILE  A key file of length exactly 16
+  -k KEY      A hexadecimal key string of length exactly 16
+  -p PASSWORD Password is used to generate an md5 hash, which is used as key
+  -f KEYFILE  A key file of length exactly 16
+  If no key, keyfile or password is provided, a password is read from stdin.
+  This requires that INFILE be specified.
 INFILE
   Path to file containing plain or cipher text
 OUTFILE
   Path to file to write
-If FILE arguments is omitted, stdin & stdout are used."""
+If either FILE argument is omitted, stdin/stdout is used."""
   def usage(msg=None):
     if msg: print >>sys.stderr, 'Error:', msg
     print >>sys.stderr, main.__doc__
@@ -93,27 +95,35 @@ If FILE arguments is omitted, stdin & stdout are used."""
   cipher = encipher
   hex = None
   key = None
+  password = None
   import sys, getopt
-  opts,args = getopt.getopt(sys.argv[1:], "dhpk:")
+  opts,args = getopt.getopt(sys.argv[1:], "dhp:tk:")
   for o,a in opts:
     if o == '-d':
       cipher = decipher
     elif o == '-h':
       hex = True
-    elif o == '-p':
+    elif o == '-t':
       hex = False
+    elif o == '-p':
+      password = a
     elif o == '-k':
       key = a
+    elif o == '-f':
+      key = open(a, 'rb').read()
     else:
       usage()
       if (len(args) < 1) or (len(args[0]) != 16):
         usage
+  if not (key or password or args): usage()
   if not key:
-    if (not args): usage()
-    key = open(args.pop(0), 'rb').read()
+    import getpass, hashlib
+    if not password:
+      password = getpass.getpass("Password:")
+    key = hashlib.md5(password).digest()[:16]
   if len(key) != 16:
     usage('key length must be 16')
-  key = struct.unpack('4L', key)
+  key = struct.unpack('4I', key)
 
   input = args and open(args.pop(0), 'rb') or sys.stdin
   output = args and open(args.pop(0), 'wb') or sys.stdout
